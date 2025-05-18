6185122ab146401f5d576b107dd22e68613355ce
Rohit <zaydansari200524@gmail.com>
2025-05-18 15:42:11 +0530

    added bootstrap to some file

--- files/js/code.js
+++ files/js/code.js
@@ -1,35 +1,23 @@
-// REDIRECT START
+$(document).ready(function(){
+    // FOR SIDE SIDE_BAR SCROLL
+    $("#menu_icon").click(function(){
+        $(".side_bar").toggleClass("side_bar_toggle");
+        $("#menu_icon_2").css("transform", "scale(1)");
+        $("#menu_icon_2").css("left", "0px");
+        $("#menu_icon").css("transform", "scale(0)");
+    });
 
-function aadhaar(){
-    window.addEventListener("click", function(){
-        var adhar = document.getElementById("adhar")
-        window.location = "../html/aadhaar.html"
-    })
-}
-
-function pan(){
-    window.addEventListener("click", function(){
-        var pan = document.getElementById("pan")
-        window.location = "../html/pan-card.html"
-    })
-}
-
-function voting(){
-    window.addEventListener("click", function(){
-        var voting = document.getElementById("voting")
-        window.location = "../html/voting.html"
-    })
-}
-
-function gazette(){
-    window.addEventListener("click", function(){
-        var gazette = document.getElementById("gazette")
-        window.location = "../html/gazette.html"
-    })
-}
-
-// REDIRECT END
-// NAVIGATION START
+    $("#menu_icon_2").click(function(){
+        $(".side_bar").toggleClass("side_bar_toggle");
+        $("#menu_icon_2").css("transform", "scale(0)");
+        $("#menu_icon_2").css("left", "-25px");
+        $("#menu_icon").css("transform", "scale(1)");
+    });
 
 
-// NAVIGATION END
+    // FOR DROPDOWN (SERVICES_MENU)
+    $("#service_icon").click(function(){
+        $(".drop_down").toggleClass("d-none");
+    });
+
+});
